fa69f6340fb1c13027331a6ae43dc860b9f2d79e
eyad-404 <eyad2306444@miuegypt.edu.eg>
2025-06-08 05:22:01 +0300

    Update signinroute.js

--- routes/signinroute.js
+++ routes/signinroute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/signin_check');
 
-// تحقق من أن المسارات مطابقة لما في النموذج HTML
 router.route('/signin')
   .get(authController.getRegister)
   .post(authController.postRegister);
