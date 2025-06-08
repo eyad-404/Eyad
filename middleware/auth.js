46f14a68b5d8edc65b1b8239ca9a48f43cea0a9d
eyad-404 <eyad2306444@miuegypt.edu.eg>
2025-06-08 05:41:47 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,8 @@
-// middleware/auth.js
-
 function requireLogin(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.redirect('/login'); // ✅ changed from /signin
+        res.redirect('/login'); 
     }
 }
 
