bd7f91eecdfbea0b98ea4c8bb113555d68d4d345
eyad-404 <eyad2306444@miuegypt.edu.eg>
2025-06-10 16:41:52 +0300

    Delete routes directory

--- routes/signinroute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/signin_check');
-
-router.route('/signin')
-  .get(authController.getRegister)
-  .post(authController.postRegister);
-
-module.exports = router;
