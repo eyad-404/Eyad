1f362593c318c3b8dd6e9921f22b5b3cf4eb3cb3
eyad-404 <eyad2306444@miuegypt.edu.eg>
2025-11-18 18:20:52 +0200

    Create category.js

---
+++ controller/category.js
@@ -0,0 +1,17 @@
+const Product = require('../Models/product'); // هنا اسم الملف product.js
+
+exports.getProductsByCategory = async (req, res) => {
+  try {
+    const categoryName = req.params.categoryName; // زي: sauce, pastes
+    const products = await Product.find({ category: categoryName });
+
+    res.render('product_category', {
+      pageTitle: categoryName.replace(/_/g, ' '),
+      category: categoryName,
+      products
+    });
+  } catch (err) {
+    console.error('Error fetching products by category:', err);
+    res.status(500).send('Server error');
+  }
+};
